7628b49a3dbf2d630a2eb6ab985471934eea731d
Alyona Ermakova <ermakova.codes@gmail.com>
2025-03-18 02:13:15 +0300

    My second commit

--- src/HW7Switch/DetermineTheSeason.java
+++ src/HW7Switch/DetermineTheSeason.java
@@ -4,49 +4,49 @@ import java.util.Arrays;
 
 public class DetermineTheSeason {
     public static void main(String[] args) {
-        int monthDigits = 2;
-        String monthString = "";
+        theSeason(2);
+        split();
+        theSeason(4);
+        split();
+        theSeason(7);
+        split();
+        theSeason(11);
+        split();
+        theSeason(12);
+        split();
+        theSeason(15);
+        split();
+    }
+    public static void theSeason (int monthDigits)
+    { String monthString = "";
 
         switch (monthDigits) {
+            case 12:
             case 1:
-                monthString = "Зима";
-                break;
             case 2:
                 monthString = "Зима";
                 break;
             case 3:
-                monthString = "Весна";
-                break;
             case 4:
-                monthString = "Весна";
-                break;
             case 5:
                 monthString = "Весна";
                 break;
             case 6:
-                monthString = "Лето";
-                break;
             case 7:
-                monthString = "Лето";
-                break;
             case 8:
                 monthString = "Лето";
                 break;
             case 9:
-                monthString = "Осень";
-                break;
             case 10:
-                monthString = "Осень";
-                break;
             case 11:
                 monthString = "Осень";
-                break;
-            case 12:
-                monthString = "Зима";
                 break;
             default:
                 monthString = "Некорректно введенные данные - " + monthDigits;
         }
         System.out.println(monthString);
+    }
+    public static void split (){
+        System.out.println("---------------");
     }
 }
